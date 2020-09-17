d3ca75c67a3800f90fdc78f59827cf0f46c909eb
sankeerth <sankeerthnalam@gmail.com>
2020-09-17 13:02:58 +0530

    first commit

--- Labs/ISF462_lab2/programs/process.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main ()
-{
-  printf ("I am running in a process whose deatils are as follows\n");
-  printf("process id (pid) = %d, parent process id(ppid) = %d, user id(uid) = %d\n",getpid (), getppid (), getuid ());
-}
